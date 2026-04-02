6f2e68f6e457f9f1f45c226745c57adc9c8edea5
ganesh95kpm <ganesh95kpm@gmail.com>
2026-04-02 21:53:05 +0530

    modify

--- part3.java
+++ part3.java
@@ -2,7 +2,8 @@ package Access;
 
 public class part3 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello guys");
     }
     
 }
+ 
